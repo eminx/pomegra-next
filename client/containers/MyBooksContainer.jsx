c31ca7124a632731ce12b8b7378bb5a1164d75c5
Emin Durak <emin@tuta.io>
2020-03-10 23:53:28 +0530

    added sorting functionality to my books

--- client/containers/MyBooksContainer.jsx
+++ client/containers/MyBooksContainer.jsx
@@ -7,10 +7,28 @@ import {
   List,
   ListItem,
   Subnavbar,
-  Searchbar
+  Searchbar,
+  Actions,
+  ActionsGroup,
+  ActionsLabel,
+  ActionsButton,
+  Button,
+  Block
 } from 'framework7-react';
 
+const sortByMethods = [
+  'last added',
+  'book title',
+  'book author',
+  'book language',
+  'request condition'
+];
+
 class MyBooks extends Component {
+  state = {
+    sortBy: 'last added'
+  };
+
   viewBookInDetail = myBook => {
     this.$f7router.navigate('/book-detail/', {
       props: {
@@ -19,8 +37,45 @@ class MyBooks extends Component {
     });
   };
 
+  sortedBooks = () => {
+    const { myBooks } = this.props;
+    const { sortBy } = this.state;
+
+    let sortedBooks;
+
+    switch (sortBy) {
+      case 'book title':
+        sortedBooks = myBooks.sort((a, b) =>
+          a.b_title.localeCompare(b.b_title)
+        );
+        break;
+      case 'book author':
+        sortedBooks = myBooks.sort((a, b) =>
+          a.b_author.localeCompare(b.b_author)
+        );
+        break;
+      case 'request condition':
+        sortedBooks = myBooks.sort(
+          (a, b) =>
+            b.on_request - a.on_request ||
+            b.on_acceptance - a.on_acceptance ||
+            b.on_lend - a.on_lend
+        );
+        break;
+      case 'language':
+        sortedBooks = myBooks.sort((a, b) => a.b_lang.localeCompare(b.b_lang));
+        break;
+      default:
+        sortedBooks = myBooks.sort((a, b) => b.date_added - a.date_added);
+    }
+
+    return sortedBooks;
+  };
+
   render() {
-    const { currentUser, myBooks } = this.props;
+    const { sortBy } = this.state;
+
+    const sortedBooks = this.sortedBooks();
 
     return (
       <Page name="books">
@@ -34,18 +89,37 @@ class MyBooks extends Component {
             ></Searchbar>
           </Subnavbar>
         </Navbar>
+        <Block>
+          sorted by{' '}
+          <Button onClick={() => this.refs.sortActions.open()}>{sortBy}</Button>
+          <Actions ref="sortActions">
+            <ActionsGroup>
+              <ActionsLabel style={{ textAlign: 'center' }}>
+                sort books by
+              </ActionsLabel>
+
+              {sortByMethods.map(method => (
+                <ActionsButton
+                  key={method}
+                  bold={sortBy === method}
+                  onClick={() => this.setState({ sortBy: method })}
+                >
+                  {method}
+                </ActionsButton>
+              ))}
+            </ActionsGroup>
+          </Actions>
+        </Block>
 
         <List className="searchbar-not-found">
           <ListItem title="Nothing found" />
         </List>
-
         <List mediaList className="search-list searchbar-found">
-          {myBooks &&
-            myBooks.length > 0 &&
-            myBooks.map(myBook => (
+          {sortedBooks &&
+            sortedBooks.map(myBook => (
               <ListItem
                 mediaItem
-                key={myBook._id || myBook.b_title}
+                key={myBook._id}
                 link="#"
                 after={myBook.b_cat}
                 title={myBook.b_title}
